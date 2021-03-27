cc53652099449ae0a885af68ddc99c9489cf98c1
Ashley Pean <pean.ashley@gmail.com>
2021-03-26 23:27:11 -0400

    keeping up to date

--- server/server.js
+++ server/server.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+/* DATABASE CONNECTION */
+//process.env.DB_CONNECTION_STRING, 
+mongoose.connect(
+  "mongodb+srv://Apierre1:Annasophiaasdf1!1@cluster0.w8xvq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",  
+  {useNewUrlParser: true, useUnifiedTopology: true }, 
+  () => console.log('connected to db')
+)
  
+const db = mongoose.connection 
+
 const typeDefs = gql`
+  type Landlords {
+    name: String 
+    id: String 
+    city: String 
+    state: String 
+    street: String 
+    zipcode: String
+}
+
   type Query {
-    hello: String
+    getResults: Landlords
   }
+
 `;
  
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    getResults: () => ({
+      name: 'test test', 
+      id: '12345', 
+      city: 'test', 
+      state: 'test', 
+      street: 'test', 
+      zipcode: 'test'
+    })
   },
 };
  
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  context:  {
+    db
+  }
+});
  
 const app = express();
 server.applyMiddleware({ app });
